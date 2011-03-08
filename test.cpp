4dd5f0681c546b07604ccb8cac0b58fd12d88da4
Joakim Berglund <berglund@berglundlabs.com>
2011-03-08 01:19:57 +0100

    Player names now changeable using parameters to ./run. Example: './run -p1name Jocke -p2name Marcus'.

--- test.cpp
+++ test.cpp
@@ -16,10 +16,29 @@ SDL_Event event;
 
 int main( int argc, char* args[] )
 {
+	std::string player_one_name = "Player One";
+	std::string player_two_name = "Player Two";
+
+	 std::cout << "argc = " << argc << std::endl;
+	   for(int i = 0; i < argc; i++)
+		   std::cout << "args[" << i << "] = " << args[i] << std::endl;
+
+	   for(int i = 0; i < argc; i++) {
+		   bool p1equal = (strcmp(args[i], "-p1name") == 0);
+		   if(p1equal) {
+			   player_one_name = std::string(args[++i]);
+		   }
+		   bool p2equal = (strcmp(args[i], "-p2name") == 0);
+		   if(p2equal) {
+			   player_two_name = std::string(args[++i]);
+		   }
+
+	   }
+
     GameEngine ge(FRAMES_PER_SECOND);
 
-    Dot *dot = new Dot(100, 50, SDLK_LEFT, SDLK_RIGHT, "dot.bmp", 1, "Player one");
-    Dot *dot2 = new Dot(400, 50, SDLK_a, SDLK_d, "dot_blue.bmp", 2, "Player two");
+    Dot *dot = new Dot(100, 50, SDLK_LEFT, SDLK_RIGHT, "dot.bmp", 1, player_one_name);
+    Dot *dot2 = new Dot(400, 50, SDLK_a, SDLK_d, "dot_blue.bmp", 2, player_two_name);
     Square *platform = new Square(10, 500, 100, 10);
     Square *platform1 = new Square(190, 500, 100, 10);
     Square *platform2 = new Square(490, 500, 100, 10);
